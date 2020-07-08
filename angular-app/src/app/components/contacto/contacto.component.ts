ebbe31737257858f9222c3ae88261cfb655fb7e4
Hector <nandohector1@gmail.com>
2020-07-08 13:45:54 -0500

    pequeño cambio

--- angular-app/src/app/components/contacto/contacto.component.ts
+++ angular-app/src/app/components/contacto/contacto.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CorreoService } from '../../services/correo.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Usuario } from 'src/app/models/policy.model';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-contacto',
@@ -13,7 +15,16 @@ export class ContactoComponent implements OnInit {
   datos: any;
   mensaje: string;
   asunto: string;
-  constructor(private correo: CorreoService) {
+  usuarioLogueado: Usuario = {
+    uid: '',
+    nombre: '',
+    rol: '',
+    ApPaterno: '',
+    ApMaterno: '',
+    correo: '',
+    contraseña: '',
+  };
+  constructor(private correo: CorreoService, private servUsrs: UsuariosService) {
     this.forma = new FormGroup({
       'mensaje': new FormControl(),
       'asunto': new FormControl(),
@@ -21,13 +32,14 @@ export class ContactoComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.usuarioLogueado = this.servUsrs.optenerUsuario();
   }
 
   enviarCorreo(): void {
     console.log('EnviadoCorreo');
     this.mensaje = this.forma.get('mensaje').value;
     this.asunto = this.forma.get('asunto').value;
-    let urlapi = `https://api-palanaeum.herokuapp.com/enviar/${this.mensaje}/${this.asunto}`;
+    let urlapi = `https://api-palanaeum.herokuapp.com/enviar/${this.mensaje}/${this.asunto}/${this.usuarioLogueado.correo}`;
     this.correo.enviarCorreo(urlapi).subscribe((data) => {
       console.log(data);
       this.datos = data;
